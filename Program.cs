5da27c73a8e5b7aff9dd90731570b8d835c28875
Emil Nikolov <emil_nikolov85@abv.bg>
2025-03-31 17:11:59 +0300

    Configuration change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+
+
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
